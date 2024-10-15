30e6f4687e4b77527dc0a5f8f4bfe8b2dc09499a
YONGHUNI <dydgns0556@gmail.com>
2024-10-16 01:23:07 +0900

    added GH Actions

--- renv.R
+++ renv.R
@@ -4,8 +4,10 @@ library("httr")
 library("lubridate")
 
 
-#setwd("C:/Users/dydgn/OneDrive/Prof_Yoo/21_PAnotify")
+setwd("C:/Users/dydgn/OneDrive/Prof_Yoo/21_PAnotify")
+
 #renv::activate(profile = "packages")
+#renv::status()
 #renv::snapshot()
 #renv::deactivate(clean = F)
 #renv::deactivate(clean = T)
