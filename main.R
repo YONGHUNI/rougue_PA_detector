27b85bda8f13a342c95485db48db9cca70f0151e
YONGHUNI <dydgns0556@gmail.com>
2024-10-16 01:09:32 +0900

    first commit

---
+++ main.R
@@ -0,0 +1,98 @@
+
+# Get Sensor History
+## https://api.purpleair.com/#api-sensors-get-sensor-history
+
+get_sensor_history <- function(secret, sensor_idx, read_key = NA, privacy = NA,
+                               start_timestamp = NA, end_timestamp = NA, average = NA,
+                               fields ){
+    
+    require("data.table")
+    require("httr")
+    require("lubridate")
+    
+    
+    concat_fields <- paste(fields, collapse = ',')
+    
+    if(grepl(" ", concat_fields, fixed = T)) stop("Field param error...\n Should be an R character vector(array)")
+    
+    parse_date_time(start_timestamp, orders = "%Y-%m-%d %H:%M:%S", exact = TRUE) |> as.integer() -> stime
+    parse_date_time(end_timestamp, orders = "%Y-%m-%d %H:%M:%S", exact = TRUE) |> as.integer() -> etime
+    
+    qlist <-  Filter(Negate(anyNA),list(
+        `read_key` = read_key,      # 
+        `privacy` = privacy,  # 
+        `start_timestamp` = stime,
+        `end_timestamp` = etime,
+        `average` = average,
+        `fields` = concat_fields
+    ))
+    
+    
+    # calling an API
+    apiResult <- httr::GET( 
+        url = paste0("https://api.purpleair.com/v1/sensors/",sensor_idx,"/history"),
+        add_headers(`X-API-Key` = secret),
+        query = qlist
+    )
+    
+    
+    if (apiResult$status_code == "200"){
+        cat("ResultCode OK!\n")
+        temp <- rawToChar(apiResult$content)
+        Encoding(temp) <- "UTF-8"
+        temp <- jsonlite::fromJSON(temp)
+        
+        if (is.null(temp$data[1][[1]])) stop("No Returned Entities! Check the parameter again!") 
+        
+        result <- temp$data |> as.data.table() |> `colnames<-`(temp$fields)
+        
+        
+        result[, c("date","time") := data.table::IDateTime(as.POSIXlt(time_stamp,tz = "UTC"))] |>
+            setorderv( c("date","time"))
+        
+        
+        return(result)
+        
+        
+    }else{
+        
+        temp <- rawToChar(apiResult$content) |> jsonlite::fromJSON()
+        cat(sep = "",
+            "Bad Request Exception in / Unexpected Error\n",
+            "Status code: ",apiResult$status_code, "\n",
+            temp$error,": ",temp$description,"\n"
+        )
+        
+    }
+    
+    
+    
+    
+}
+
+
+nowPA <- function(offset = 300){
+    
+    gsub("\\..*","",as.character(now(tzone = "UTC")-offset))
+}
+
+
+
+
+
+
+
+# Import the API key
+secret <- readLines("./data/secret/secret.txt")
+sensor_idx <- readLines("./data/secret/sensor_idx.txt")
+read_key <- readLines("./data/secret/readkey.txt")
+
+
+
+
+# the simplest call
+test <- get_sensor_history(secret = secret, sensor_idx = sensor_idx, start_timestamp = nowPA(18000),
+                           read_key = read_key,  fields ="pa_latency")
+
+
+
---
+++ renv.R
@@ -0,0 +1,13 @@
+
+library("data.table")
+library("httr")
+library("lubridate")
+
+
+#setwd("C:/Users/dydgn/OneDrive/Prof_Yoo/21_PAnotify")
+#renv::activate(profile = "packages")
+#renv::snapshot()
+#renv::deactivate(clean = F)
+#renv::deactivate(clean = T)
+
+
