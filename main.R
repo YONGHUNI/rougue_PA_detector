57ab9e1f3e17d8670bb4b5160ce9fd232cba6367
YONGHUNI <dydgns0556@gmail.com>
2024-11-04 23:13:25 +0900

    feat(main.R): Added support for multiple sensors & deprecated GH issuer

--- main.R
+++ main.R
@@ -76,64 +76,101 @@ nowPA <- function(offset = 300){ # default value: 5 minutes
     gsub("\\..*","",as.character(now(tzone = "UTC")-offset))
 }
 
-# readxl::read_xlsx(path = "./data/secret/key2Yonghun_1010.xlsx",sheet = 2)
-# 
-# jsonlite::toJSON(readxl::read_xlsx(path = "./data/secret/key2Yonghun_1010.xlsx",sheet = 2))|>
-#     charToRaw() |>
-#     base64enc::base64encode()|>
-#     writeLines("./data/secret/participant.txt")
 
 
 
-# Import the API key
-# secret <- readLines("./data/secret/secret.txt")
-# sensor_idx <- readLines("./data/secret/sensor_idx.txt")
-# read_key <- readLines("./data/secret/readkey.txt")
-
-ghtoken <- Sys.getenv("TOKEN_GH")
-
-
-secret <- Sys.getenv("SECRET")
-sensor_idx <- Sys.getenv("SENSOR_IDX")
-read_key <- Sys.getenv("READ_KEY")
-
-#jsonlite::read_json("./data/secret/participants.json")
-
-
-#print(getwd())
+ 
+  # jsonlite::toJSON(readxl::read_xlsx(path = "./data/secret/key2Yonghun_1012.xlsx",sheet = 2))|>
+  #     charToRaw() |>
+  #     base64enc::base64encode()|>
+  #     writeLines("./data/secret/participant.txt")
 
 
 
-tryCatch({
+if (Sys.info()[[1]]=="Windows") {
     
-    sensor_data <- get_sensor_history(secret = secret,
-                                      sensor_idx = sensor_idx,
-                                      start_timestamp = nowPA(),
-                                      read_key = read_key,
-                                      average = 0,
-                                      fields ="pa_latency"
-                                      )
+    # For my Windows Environment
+    # Import the API key
+    secret <- readLines("./data/secret/secret.txt")
+    sensor_idx <- readLines("./data/secret/sensor_idx.txt")
+    read_key <- readLines("./data/secret/readkey.txt")
+    database <- readLines("./data/secret/participant.txt") |>
+        base64enc::base64decode() |>
+        rawToChar() |>
+        jsonlite::fromJSON()
     
-    cat(lubridate::now(tzone = "UTC")|>as.character(),"\n")
-    print(sensor_data[,c("date","time")])
+} else{
+    
+    # For github actions Ubuntu env
+    
+    secret <- Sys.getenv("SECRET")
+    database <- Sys.getenv("DATABASE")
     
     
+    #deprecated
+    #ghtoken <- Sys.getenv("TOKEN_GH")
+    #sensor_idx <- Sys.getenv("SENSOR_IDX")
+    #read_key <- Sys.getenv("READ_KEY")
     
-}, error = function(e) {
-    
-    cat("Issuing on GitHub\n")
-    msg <- paste0("[Bot] ",nowPA(), " UTC : Warning! Offline Sensor detected! (Sensor Index: ", sensor_idx,")")
+}
 
-    gh::gh(
-        .token = ghtoken,
-        endpoint = "POST /repos/YONGHUNI/rougue_PA_detector/issues",
-        title = msg,
-        body = paste0(nowPA(), "UTC : Warning! Offline Sensor detected!\n (Sensor Index: ", sensor_idx,")\n@YONGHUNI please respond!")
-    )
+rogue_sensors <-  data.table()
+
+
+# 12 sensors + Dr.Yoo's sensor
+
+for (i in 1:length(database$`sensor index`)) {
+    
+    
+    tryCatch({
+        
+        sensor_data <- get_sensor_history(secret = secret,
+                                          sensor_idx = database$`sensor index`[i],
+                                          start_timestamp = nowPA(),
+                                          read_key = database$`read key`[i],
+                                          average = 0,
+                                          fields ="pa_latency"
+        )
+        
+        cat(lubridate::now(tzone = "UTC")|>as.character(),"\n")
+        print(sensor_data[,c("date","time")])
+        
+        
+        
+    }, error = function(e) {
+        
+        # for debugging
+        cat(paste0(i,"\n"))
+        
+        ######## deprecated
+        # gh::gh(
+        #     .token = ghtoken,
+        #     endpoint = "POST /repos/YONGHUNI/rougue_PA_detector/issues",
+        #     title = msg,
+        #     body = paste0(nowPA(), "UTC : Warning! Offline Sensor detected!\n (Sensor Index: ", sensor_idx,")\n@YONGHUNI please respond!")
+        # )
+        
+        rogue_sensors <- rbind(database[i,],rogue_sensors)
+        
+        
+    })
+    
+}
+
+
+if (nrow(rogue_sensors)!=0) {
+    
+    cat("Issuing on Discord\n")
+    msg <- paste0("[Bot] ", 
+                  nowPA(), 
+                  " UTC : Warning! Offline Sensor detected!\n\tSensor Index:\n\t\t> ",
+                  paste(rogue_sensors$`sensor index`,collapse = "\n\t\t> "),
+                  "\nPlease refer to the CSV file attached.")
+    
     writeLines(msg, "./msg")
-    #write.csv() # for handling multiple sensors
-})
+    fwrite(rogue_sensors,"./list.csv")
+    
+}
 
 
-
-
+  
